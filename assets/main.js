1ad435dd1ab122aa79e2d4a1f0bdfc42fa0252a9
Sabrinashh <110469877+Sabrinashh@users.noreply.github.com>
2022-10-11 22:59:14 +0400

    file

--- assets/main.js
+++ assets/main.js
@@ -1,8 +1,14 @@
 let username=[];
-let input1=prompt(username)
+let input1=prompt("Daxil edin")
+if(input1!=null && isNaN(input1)){
+}  
+else{
+    alert("Duzgun daxil edin")
+}
+username.push(input1)
 let input=prompt("Username daxil edin")
 if(input!=null && isNaN(input)){
-    if(input!=input1)
+    if(input!=username)
     alert("Username ugurla elave olundu")
 else{
     alert("Bele Username movcutdur")
